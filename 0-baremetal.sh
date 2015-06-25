5215ae4278a93f963407f8d25d77eedf1ac5ee94
Shannon Quinn <magsol@gmail.com>
2015-06-25 14:24:52 -0400

    Added new packages to install, as well as updated configuration for Hadoop and Mesos.

--- 0-baremetal.sh
+++ 0-baremetal.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# This script is run on the bare metal.
+# This script is run on the bare metal. Needs admin privileges, though.
+if [ "$(whoami)" != "root" ]; then
+    echo "Must be run with sudo."
+    exit 1
+fi
 
 # Step 0: Let's define some variables.
 HADOOP_VERSION=2.7.0
@@ -11,36 +15,36 @@ MESOS_PREFIX=/opt/mesos
 # Step 1: Perform the apt updates and install Docker.
 apt-get -y update
 apt-get -y install build-essential wget openjdk-7-jdk python-dev python-boto \
-    libcurl4-nss-dev libsasl2-dev maven libapr1-dev libsvn-dev
+    libcurl4-nss-dev libsasl2-dev maven libapr1-dev libsvn-dev openssh-server \
+    rsync
 wget -qO- https://get.docker.com/ | sh
 
-# Step 2: Install Docker Compose (or decking).
-# wget https://bootstrap.pypa.io/get-pip.py
-# python get-pip.py
-# pip install -U docker-compose
+# Step 2: Install the various configuration files and environment variables:
+# - /etc/hosts
+# - bashrc
+# - Potentially SSH configuration later, if needed
+cp config/etc/hosts /etc/
+cp config/bashrc.sh ~/.bashrc
 
-# Step 2: Install Hadoop. We're not strictly using Hadoop MapReduce so much
+# Step 3: Install Hadoop. We're not strictly using Hadoop MapReduce so much
 # as HDFS; this is where we store the data long-term. Containers are just
 # for ephemeral analysis. Which means we also need to configure Hadoop.
 wget http://mirrors.sonic.net/apache/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz
 tar zxvf hadoop-$HADOOP_VERSION.tar.gz
 rm hadoop-$HADOOP_VERSION.tar.gz
 mv hadoop-$HADOOP_VERSION $HADOOP_PREFIX
+cp config/hadoop/etc/hadoop/* $HADOOP_PREFIX/etc/hadoop/  # Copy the config files over.
 
-## TODO: Make a .bashrc supplement with the needed Hadoop environment vars.
-
-# Step 3: Install Mesos.
+# Step 4: Install Mesos.
 wget http://www.apache.org/dist/mesos/$MESOS_VERSION/mesos-$MESOS_VERSION.tar.gz
 tar zxvf mesos-$MESOS_VERSION.tar.gz
 rm mesos-$MESOS_VERSION.tar.gz
 mkdir mesos-$MESOS_VERSION/build
+
 cd mesos-$MESOS_VERSION/build
 ../configure --prefix=$MESOS_PREFIX
-# make -j 8 && make check && make install
-# chmod +x /usr/local/etc/mesos/*.template
-# mv /usr/local/etc/mesos/mesos-deploy-env.sh.template /usr/local/etc/mesos/mesos-deploy-env.sh
-# mv /usr/local/etc/mesos/mesos-master-env.sh.template /usr/local/etc/mesos/mesos-master-env.sh
-# mv /usr/local/etc/mesos/mesos-slave-env.sh.template /usr/local/etc/mesos/mesos-slave-env.sh
-
+make -j 8 && make check && make install
+cd -
+cp -r config/mesos/var/ $MESOS_PREFIX/
 
 # Step 3: Pull down the Docker image and get to work.
