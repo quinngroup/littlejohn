d0da6aa794bb05b3b54deaf7da4aa12104588897
Shannon Quinn <magsol@gmail.com>
2015-07-13 15:48:33 -0400

    Found the fix--needed to mount a couple of volumes in the mesos-slave startup. Works perfectly.

--- dockerfiles/lj-mesos-slave/start.sh
+++ dockerfiles/lj-mesos-slave/start.sh
@@ -2,4 +2,5 @@ docker run -d \
     --net="host" \
     -p 31000-31050:31000-31050 \
     -e "MESOS_MASTER=zk://1.1.1.1:2181/mesos" \
+    -v /sys:/sys -v /var/run/docker.sock:/var/run/docker.sock \
     magsol/lj-mesos-slave
