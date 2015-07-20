f14ae94ac92115956464c1c45e8ef44bdb5d482d
Shannon Quinn <magsol@gmail.com>
2015-07-19 20:29:00 -0400

    Started work on the Spark base.

--- dockerfiles/lj-spark-base/config/bootstrap/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# runsvdir-start clears environment variables
-# initially export all variables (later reimport within /run file)
-export > /env.bash
-
-# start service
-runsvdir-start
