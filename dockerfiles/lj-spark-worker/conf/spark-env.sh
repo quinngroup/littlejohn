695668b6ea66c3b495e72a0d3eaec64c8919982d
Shannon Quinn <magsol@gmail.com>
2015-07-22 18:19:14 -0400

    Moved Spark configuration into the worker and master.

--- dockerfiles/lj-spark-worker/conf/spark-env.sh
+++ dockerfiles/lj-spark-worker/conf/spark-env.sh
@@ -3,6 +3,8 @@
 # This file is sourced when running various Spark programs.
 # Copy it as spark-env.sh and edit that to configure Spark for your site.
 
+export MESOS_NATIVE_JAVA_LIBRARY=/opt/mesos/libmesos.so
+
 # Options read when launching programs locally with
 # ./bin/run-example or ./bin/spark-submit
 # - HADOOP_CONF_DIR, to point Spark towards Hadoop configuration files
